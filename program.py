1d19a8f4081e1e23d806b83cc112de69d764456a
amarnathkhamkar <amarnath.khamkar@gmail.com>
2022-12-10 12:20:36 +0530

    update program file
    basics of python

---
+++ program.py
@@ -0,0 +1,144 @@
+print("Hello World")
+
+
+
+print(12)
+
+print(3*3)
+
+print("@12")
+
+print('Apple')
+
+50
+
+print('''Amar''')
+
+"scaler"
+
+True
+
+False
+
+a=5;
+print(a)
+
+a=5
+
+a
+
+print(a)
+
+type(a)
+
+print(type(a))
+
+isinstance(a,int)
+
+isinstance([1,2,3], object)
+
+isinstance (print,object)
+
+w=10
+
+w="amar"
+
+w
+
+x="scaler"
+
+
+a=56
+
+id(a)
+
+b=56
+
+id(b)
+
+a
+
+id(x)
+
+z="Scaler"
+
+id(z)
+
+e="Scaler"
+
+id(e)
+
+print(isinstance(print,object))
+
+isinstance(print,object)
+
+lis = [(0, 2), (1, 3), (2, 4)]
+result = sum (n for _, n in lis)
+print(result)
+
+s="ABC"
+s[::-1]
+
+string= "am a not a in a mood"
+
+def unknown(string):
+    a = a + string
+    a="".join(a)
+    print(a)
+
+a= "a amar a is a boy"
+b= list(a)
+b="".join(a)
+b
+
+b="".join(a)
+b
+
+a= "amarnath"
+b= set(a)
+b
+
+sampleDict = { "class":{ "student":{ "name":"Mike", "marks":{ "physics":70, "history":80 } } } }
+sampleDict
+
+my_string="amarnathm"
+k = [i for i in my_string if i not in "aeiou"] 
+
+print(k)
+
+
+
+import keyword
+print(keyword.kwlist)
+
+int
+
+isinstance(amar,str)
+
+3+3
+
+10-5
+
+5*2
+
+5**2
+
+25/2
+
+10/3
+
+8//3
+
+25//4
+
+-25//4
+
+25/3
+
+25//3
+
+5%2
+
+11%3
+
+
